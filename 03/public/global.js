17120b4bec924dcde7ae1c54e76ac7f54df7c5b3
Lars Hollingsed <larshollingsed@gmail.com>
2015-07-12 21:51:59 -0500

    03 - Exercise 1 completed

--- 03/public/global.js
+++ 03/public/global.js
@@ -1,19 +1,19 @@
 var req = new XMLHttpRequest();
 
-req.open("get", "/path1");
 
-req.addEventListener("load", function(){
-  alert(this.response);
-});
-
-req.send();
 
 //  ---------------------------------------------------------------------------
 
 var myLink = document.getElementById("clicker");
 
 var handleTheClick = function(event){
-  // ?????????
+  req.open("get", "/path1");
+
+  req.addEventListener("load", function(){
+    alert(this.response);
+  });
+
+  req.send();
   
   // Prevent the link from refreshing the DOM.
   event.preventDefault();
