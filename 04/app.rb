071c070e9ed04f59aa79dacbc5d1191e8c27e5a7
Lars Hollingsed <larshollingsed@gmail.com>
2015-07-12 22:39:12 -0500

    04 - Exercise 04 completed

--- 04/app.rb
+++ 04/app.rb
@@ -6,6 +6,13 @@ get "/" do
 end
 
 get "/:msg" do
-  @message = params["msg"]
+  message = params["msg"]
+  if message.to_i != 0
+    @response_message = "You wrote #{message}.  Is that how old you are?"
+  elsif message == message.capitalize
+    @response_message = "You wrote #{message}.  Is that someone's name?"
+  else 
+    @response_message = "You wrote #{message}.  Whateva'."
+  end
   erb :"message"
 end
