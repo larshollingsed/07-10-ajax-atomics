3c0988813f168127d80138e5006fc909e3331f78
Lars Hollingsed <larshollingsed@gmail.com>
2015-07-12 21:33:25 -0500

    01 - Exercise 3 complete

--- 01/public/global.js
+++ 01/public/global.js
@@ -1,14 +1,18 @@
 var req = new XMLHttpRequest();
 
 // For 'Exercise 2', you will modify this line:
-req.open("get", "/info2");
+req.open("get", "/info1");
 
 req.addEventListener("loadstart", function(){
   console.log("loadstart");
 });
 
 req.addEventListener("load", function(){
-  alert(req.response);
+  if (req.response === "yes") {
+    alert("Yay!")
+  } else {
+    alert("Aw, shucks!")
+  }
 });
 
 req.send();
