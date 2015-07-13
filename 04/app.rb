99d97c896a9d454a65b41c715d52f8b0bbc65d34
Lars Hollingsed <larshollingsed@gmail.com>
2015-07-12 22:29:56 -0500

    04 - Exercise 03 completed

--- 04/app.rb
+++ 04/app.rb
@@ -1,4 +1,5 @@
 require "sinatra"
+require "pry"
 
 get "/" do
   erb :"home"
@@ -6,5 +7,11 @@ end
 
 get "/:msg" do
   message = params["msg"]
-  return "You wrote #{message}"
+  if message.to_i != 0
+    return "You wrote #{message}.  Is that how old you are?"
+  elsif message == message.capitalize
+    return "You wrote #{message}.  Is that someone's name?"
+  else 
+    return "You wrote #{message}.  Whateva'."
+  end
 end
